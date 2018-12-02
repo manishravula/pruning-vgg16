967b2777faab661bac85f8f3e5510ccab1038308
Manish Ravula <manish97ravula@gmail.com>
2018-12-02 06:48:02 +0000

    cfarvgg.py

--- cfarvgg.py
+++ cfarvgg.py
@@ -119,7 +119,7 @@ def test(epoch):
             total += targets.size(0)
             correct += predicted.eq(targets).sum().item()
 
-            progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d) % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+            progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)' % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
 
     # Save checkpoint.
     acc = 100.*correct/total
